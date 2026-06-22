1d2d48a969437ee12caf8f9b9e33a491eb3246d9
Myagmarbat <mdorjtse@Myagmarbats-MacBook-Pro.local>
2026-06-22 11:52:43 -0700

    add deploy setup

--- app/config.py
+++ app/config.py
@@ -7,5 +7,6 @@ APP_NAME = os.getenv("APP_NAME", "event-api")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Path to the CA certificate file for DigitalOcean Managed PostgreSQL.
-# In CI/production, write the DO_DB_CA_CERT secret to a temp file and set this to that path.
+# In CI/production, write the DO_DB_CA_CERT secret to a temp file and set
+# this to that path.
 DB_SSL_CA = os.getenv("DB_SSL_CA")
