9a7f36ad2b08a6773c87f6d914505df5ea460ca4
Myagmarbat <mdorjtse@Myagmarbats-MacBook-Pro.local>
2026-06-21 20:27:17 -0700

    lint fix

--- app/service.py
+++ app/service.py
@@ -1,9 +1,14 @@
 import uuid
 from app.models import Event
 
+
+def _default_id_generator() -> str:
+    return str(uuid.uuid4())
+
+
 def create_event_object(event_type: str, user_id: str, id_generator=None) -> Event:
     if id_generator is None:
-        id_generator = lambda: str(uuid.uuid4())
+        id_generator = _default_id_generator
 
     return Event(
         id=id_generator(),
