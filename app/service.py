4758873b130ff7c7d342167ce5d15b94eafe0ecf
Myagmarbat <mdorjtse@Myagmarbats-MacBook-Pro.local>
2026-06-21 18:34:16 -0700

    test fix

--- app/service.py
+++ app/service.py
@@ -1,10 +1,12 @@
 import uuid
 from app.models import Event
 
+def create_event_object(event_type: str, user_id: str, id_generator=None) -> Event:
+    if id_generator is None:
+        id_generator = lambda: str(uuid.uuid4())
 
-def create_event_object(event_type: str, user_id: str):
     return Event(
-        id=str(uuid.uuid4()),
+        id=id_generator(),
         event_type=event_type,
-        user_id=user_id
+        user_id=user_id,
     )
